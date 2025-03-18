098a5932e61bcf0a921c026bffb198b75384aed1
Tiger Abrodi <tigerabrodi@gmail.com>
2025-03-18 04:45:23 +0100

    toast, packages, font

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,20 @@
+import { Route, Routes } from 'react-router'
+import { AuthenticatedLayout } from './layouts/authenticated'
+import { ROUTES } from './lib/constants'
+import { DashboardPage } from './pages/dashboard'
+import { LoginPage } from './pages/login'
+import { SettingsPage } from './pages/settings'
+import { StoryDetailPage } from './pages/story-detail'
+
 export function App() {
-  return <div>hello world</div>;
+  return (
+    <Routes>
+      <Route path={ROUTES.login} element={<LoginPage />} />
+      <Route element={<AuthenticatedLayout />}>
+        <Route path={ROUTES.dashboard} element={<DashboardPage />} />
+        <Route path={ROUTES.storyDetail} element={<StoryDetailPage />} />
+        <Route path={ROUTES.settings} element={<SettingsPage />} />
+      </Route>
+    </Routes>
+  )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { App } from "./App.tsx";
-import "./index.css";
+import { Toaster } from '@/components/ui/sonner'
+import { ConvexAuthProvider } from '@convex-dev/auth/react'
+// @ts-expect-error - fontsource-variable/inter is not typed
+import '@fontsource-variable/inter'
+import { Analytics } from '@vercel/analytics/react'
+import { ConvexReactClient } from 'convex/react'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router'
+import { App } from './App.tsx'
+import './index.css'
 
-createRoot(document.getElementById("root")!).render(
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string)
+
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ConvexAuthProvider client={convex}>
+      <BrowserRouter>
+        <App />
+        <Toaster position="top-right" />
+        <Analytics />
+      </BrowserRouter>
+    </ConvexAuthProvider>
   </StrictMode>
-);
+)
