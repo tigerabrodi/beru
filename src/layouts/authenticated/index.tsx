7814c15def5438b6c860af4c9e992412cf94ba42
Tiger Abrodi <tigerabrodi@gmail.com>
2025-03-18 04:57:33 +0100

    auth check on layout

--- src/layouts/authenticated/index.tsx
+++ src/layouts/authenticated/index.tsx
@@ -1,9 +1,33 @@
 import { SidebarProvider } from '@/components/ui/sidebar'
-import { Outlet } from 'react-router'
+import { ROUTES } from '@/lib/constants'
+import { api } from '@convex/_generated/api'
+import { useConvexAuth, useQuery } from 'convex/react'
+import { Loader2 } from 'lucide-react'
+import { useEffect } from 'react'
+import { generatePath, Outlet, useNavigate } from 'react-router'
 import { AppSidebar } from './components/app-sidebar'
 import { SidebarTriggerFixed } from './components/sidebar-trigger-fixed'
 
 export function AuthenticatedLayout() {
+  const user = useQuery(api.users.queries.getCurrentUser)
+  const state = useConvexAuth()
+  const isLoading = user === undefined || state.isLoading
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (!isLoading && !user) {
+      void navigate(generatePath(ROUTES.login))
+    }
+  }, [isLoading, user, navigate])
+
+  if (isLoading) {
+    return (
+      <div className="flex flex-1 items-center justify-center">
+        <Loader2 className="size-10 animate-spin" />
+      </div>
+    )
+  }
+
   return (
     <SidebarProvider>
       <div className="flex h-screen w-full">
